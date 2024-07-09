7eb3e7a33f5c3c1309c9672f8daa58bdccb3b4d3
Ankit-y45h <ankityash03@gmail.com>
2024-07-09 18:56:32 +0530

    Form Handling: Controlled component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,58 @@
-import React, { createRef } from 'react';
+import React from 'react';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
 
-    this.emailInputRef = React.createRef();
-    this.passwordInputRef = React.createRef();
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
 
-  handleFormSubmit=(e)=>{
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log('this.emailInputRef',this.emailInputRef);
-    console.log('this.passwordInputRef',this.passwordInputRef);
-  }
+    // console.log('this.emailInputRef',this.emailInputRef);
+    // console.log('this.passwordInputRef',this.passwordInputRef);
+    console.log('this.state', this.state);
+  };
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
         <div className="field">
-          <input type="email" placeholder="e-mail" required ref={this.emailInputRef}/>
+          <input
+            type="email"
+            placeholder="e-mail"
+            required
+            //ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="password" required ref={this.passwordInputRef}/>
+          <input
+            type="password"
+            placeholder="password"
+            required
+            //ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+          />
         </div>
         <div className="field">
           <button onClick={this.handleFormSubmit}>Log In</button>
